a435d62405b10bce9c9dab00d19e0a8c265a851e
Maud de Vries <emcoding@pm.me>
2018-12-20 21:26:31 +0100

    Version in medium post
    Start with: $ rails new appname -d postgresql -m ../template_5_2.rb

---
+++ template_5_2.rb
@@ -0,0 +1,28 @@
+puts 'Template_5_2'
+puts 'My first template, by Maud de Vries'
+
+# gems
+gem 'webpacker', '~> 3.5', '>= 3.5.3'
+gem_group :development, :test do
+  gem 'better_errors'
+end
+
+gsub_file "Gemfile", /^gem\s+["']coffee-rails["'].*$/, "# Todo Remove coffeescript comment ^"
+
+append_to_file "Gemfile", "# TODO Fix gemfile. Template messed it up :-( "
+
+# things
+generate(:scaffold, "thing title:string")
+route "root 'things#index'"
+
+# database
+rails_command("db:create")
+rails_command("db:migrate")
+
+# installs after bundle
+after_bundle do
+  rails_command "webpacker:install"
+  git add: "."
+  git commit: %Q{ -m 'Init commit with template_5_2' }
+end
+
